08a677b3ac8f7d4f19b48df4d21d9fef9c98e7a7
Sanjay-2000 <sanjay.space.66@gmail.com>
2021-04-07 10:21:36 +0530

    tasklist

--- lib/Widgets/task_list.dart
+++ lib/Widgets/task_list.dart
@@ -1,28 +1,24 @@
 import 'package:flutter/material.dart';
 import 'text_tile.dart';
+import 'package:to_day/modals/task.dart';
+import 'package:to_day/Widgets/text_tile.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+List<Task> tasks = [
+  Task(name: 'Meditate for 20 mins'),
+  Task(name: 'Meditate for 20 mins'),
+  Task(name: 'Meditate for 20 mins'),
+];
+
+class _TaskListState extends State<TaskList> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        Texttile(),
-        SizedBox(
-          height: 20,
-        ),
-      ],
-    );
+    return ListView.builder(itemBuilder: (context, index) {
+      return Texttile();
+    });
   }
 }
