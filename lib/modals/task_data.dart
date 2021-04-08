c42a9a4ef78f1ebe9a2a7b762c1e73cea0deb58e
Sanjay-2000 <sanjay.space.66@gmail.com>
2021-04-08 17:09:13 +0530

    taskdata model

--- lib/modals/task_data.dart
+++ lib/modals/task_data.dart
@@ -1,15 +1,37 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:to_day/modals/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'tasks'),
     Task(name: 'tasks1'),
     Task(name: 'tasks2'),
     Task(name: 'tasks'),
   ];
 
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
   }
 }
